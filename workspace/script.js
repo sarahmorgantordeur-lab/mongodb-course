eb2718bbd1d9906f27e93fb02a21477f34543ba8
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 13:34:01 +0100

    exemple avec _id: false

--- workspace/script.js
+++ workspace/script.js
@@ -14,3 +14,20 @@ const movies = db.movies
 
 console.log(movies);
 
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+// USE technocite
+db = db.getSiblingDB('sample_mflix');
+
+const movies = db.movies
+.find({
+    released:  {
+        $exists: false
+    }
+})
+.projection({
+    title: true,
+    _id: false
+});
+
+console.log(movies);
+
