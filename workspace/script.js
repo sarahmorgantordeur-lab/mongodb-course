ef54009256bddb5b673c629819b1be0063650a2e
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 11:51:26 +0100

    aller chercher des données avec find

--- workspace/script.js
+++ workspace/script.js
@@ -2,26 +2,6 @@ let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 // USE technocite
 db = db.getSiblingDB('technocite');
 
-// INSERT INTO students (name) VALUES ("Jean Sébastien")
-const inserted = db.students.insertOne({
-    name: "Jean Sébastien"
-});
+const students = db.students.find();
 
-console.log(inserted);
-
-const multiInsert = db.students.insertMany([
-    {
-        name : "Jammy"
-    },
-    {
-        name : "Fred"
-    }
-]);
-
-console.log(multiInsert);
-
-const myBoss = db.formators.insertOne({
-    name: "Gilles Bertrand"
-});
-
-console.log(myBoss);
+console.log(students);
