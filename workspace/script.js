4a446c4a5a8d3e7b5684ad19d8febc50b5fe737c
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 13:16:31 +0100

    notation à .

--- workspace/script.js
+++ workspace/script.js
@@ -2,10 +2,24 @@ let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 // USE technocite
 db = db.getSiblingDB('technocite');
 
-const students = db.students.find({
-    name:{
+
+let students = db.students.find({
+    name: {
         $ne: "Fred",
-        $eq: "Amaury"
+        $eq: "Jammy"
+    }
+});
+
+db.students.insertOne({
+    name: "Gerard",
+    notes: {
+        geography: 5
+    }
+})
+
+students = db.students.find({
+    "notes.geography": {
+        $eq: 5
     }
 });
 
