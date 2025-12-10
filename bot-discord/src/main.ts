718ca6f9743182ed410244238940b04b1daa5ad5
AmauryD <deflorenne.amaury@gmail.com>
2025-12-10 09:44:43 +0100

    trucs

---
+++ bot-discord/src/main.ts
@@ -0,0 +1,11 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+await client.login(process.env.DISCORD_BOT_TOKEN);
+
+console.log('Bot is running...');
--- resources/mongo/README.md
+++ resources/mongo/README.md
@@ -1,3 +1,5 @@
 # Hello there
 
 Veuillez lire les slides pour la partie mongo, pas besoin de retaper tous les bouts de code ici car ils sont assez petits et peuvent tenir sur un slide.
+
+Bande de flemmards.
