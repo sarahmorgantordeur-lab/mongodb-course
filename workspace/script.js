5d00d9af2138274b0c482bbad3172e3704e95351
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 12:00:05 +0100

    condition pour trouver <=> du where en sql mais en plus propre

--- workspace/script.js
+++ workspace/script.js
@@ -3,7 +3,9 @@ let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 db = db.getSiblingDB('technocite');
 
 const students = db.students.find({
-    name: "Fred"
+    name:{
+        $eq: "Fred"
+    }
 });
 
 console.log(students);
