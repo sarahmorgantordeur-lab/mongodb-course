8845b66a40cc2c1000092c21613c9e917a815bec
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 14:40:47 +0100

    exercices partie 1

--- workspace/script.js
+++ workspace/script.js
@@ -4,32 +4,62 @@ db = db.getSiblingDB('sample_mflix');
 
 // SELECT title FROM movies WHERE released IS NULL
 const movies = db.movies
-.find({
-    $or: [
-        {
-            $and: [
-                {
-                    year: 2025
-                },
-                {
-                    title: "Jurassic Park"
-                }
-            ]
-        },
-        {
-            year: 2020
-        }
-    ]
-})
-.projection({
-    title: true,
-    year: true,
-    _id: false
-})
-.sort({
-    year: -1
-})
-.limit(5);
+    // .find({
+    //     cast: {
+    //         $in: ["Keanu Reeves"]
+    //     }
+    // });
 
+    // .find({
+    //     genres: {
+    //         $in: ["Comedy"]
+    //     }
+    // });
+
+    // .find({
+    //     year: {
+    //         $gte: 2002, $lte: 2008
+    //     }
+    // })
+
+    // .find({
+    //     $and: [
+    //         {
+    //             cast: {
+    //                 $in: ["Chris O'Donnell"]
+    //             },
+    //             cast: {
+    //                 $in: ["Matt Damon"]
+    //             }
+    //         }
+    //     ]
+    // })
+
+    // .find({
+    //     $or: [
+    //         {directors: ["Neil Burger"]},
+    //         {directors: ["Brad Furman"]}
+    //     ]
+    // })
+
+// const movie = movies.aggregate([
+//   {
+//     $group: {
+//         _id: null,
+//         minYear: { $min: "$year" }
+//     }
+//   }
+// ])
+
+// const movie = movies.find({
+//         $and: [
+//             {"imdb.rating": {$glt: 8.0}},
+//             {"tomatoes.critic.rating": {$glt: 8}}
+//         ]
+//     })
+
+.find({
+    "realeased.date": { $exists: false }
+})
 console.log(movies);
 
