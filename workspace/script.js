316f3c3d0a917fdf0f6d31dd3797e48ffd0b028a
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 13:32:42 +0100

    renvoie le titre et l id fct projection, si veut pas id mettre id:0 ou id: false

--- workspace/script.js
+++ workspace/script.js
@@ -2,11 +2,15 @@ let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 // USE technocite
 db = db.getSiblingDB('sample_mflix');
 
-const movies = db.movies.find({
+const movies = db.movies
+.find({
     released:  {
         $exists: false
     }
-});
+})
+.projection({
+    title: true
+})
 
 console.log(movies);
 
