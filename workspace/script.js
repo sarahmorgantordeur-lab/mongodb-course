b0c9ef8cc70f2043f43b6e588ecb119a39547a76
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 11:49:02 +0100

    insert students et formateurs

--- workspace/script.js
+++ workspace/script.js
@@ -1,9 +1,27 @@
 let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+// USE technocite
+db = db.getSiblingDB('technocite');
 
-d = db.getSiblingDB("technocite");
-
+// INSERT INTO students (name) VALUES ("Jean Sébastien")
 const inserted = db.students.insertOne({
-    name: "Alice",
+    name: "Jean Sébastien"
 });
 
-console.log("Inserted document ID:", inserted);
+console.log(inserted);
+
+const multiInsert = db.students.insertMany([
+    {
+        name : "Jammy"
+    },
+    {
+        name : "Fred"
+    }
+]);
+
+console.log(multiInsert);
+
+const myBoss = db.formators.insertOne({
+    name: "Gilles Bertrand"
+});
+
+console.log(myBoss);
