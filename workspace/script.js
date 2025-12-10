c244d025c3557930b60fae843dd6e7388edbe4fc
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-12-10 11:46:13 +0100

    first

--- workspace/script.js
+++ workspace/script.js
@@ -1,38 +1,9 @@
-let db = connect("mongodb://root:test123@localhost");
-// équivalent du "use technocite"
-db = db.getSiblingDB("sample_mflix");
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 
-db.movies.aggregate([
-    {
-        $match: {
-            'imdb.rating': {
-                $lt: 5
-            }
-        }
-    },
-    {
-        $unwind: '$directors'
-    },
-    {
-        $group: {
-            _id: '$directors',
-            total: {
-                $count: {}
-            }
-        }
-    },
-    {
-        $sort: {
-            total: -1
-        }
-    },
-    {
-        $limit: 10
-    },
-    {
-        $out : {
-            db: 'sample_mflix',
-            coll: 'lame_directors'
-        }
-    }
-]);
+d = db.getSiblingDB("technocite");
+
+const inserted = db.students.insertOne({
+    name: "Alice",
+});
+
+console.log("Inserted document ID:", inserted);
