04cb7a64c2ccf60be715b947f67a46235f62f830
AmauryD <deflorenne.amaury@gmail.com>
2025-12-10 11:45:49 +0100

    théorie 1

--- workspace/script.js
+++ workspace/script.js
@@ -1,2 +1,21 @@
-db = connect("mongodb://root:test123@localhost/test?authSource=admin")
-print(db.getUsers())
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+// USE technocite
+db = db.getSiblingDB('technocite');
+
+// INSERT INTO students (name) VALUES ("Jean Sébastien")
+const inserted = db.students.insertOne({
+    name: "Jean Sébastien"
+});
+
+console.log(inserted);
+
+const multiInsert = db.students.insertMany([
+    {
+        name : "Jammy"
+    },
+    {
+        name : "Fred"
+    }
+]);
+
+console.log(multiInsert);
